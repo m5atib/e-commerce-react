496ed9f6e163f82fcbfb062e502e84e4c28c3945
Muhaimen Khatib <m5atib@gitub.com>
2022-12-18 11:44:53 +0200

    add react pages routing v6

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,18 @@
-import React from 'react';
-import './App.css';
-
+import React from "react";
+import "./App.css";
+import ShoppingList from "./Pages/ShoppingList";
+import { Routes, Route } from "react-router-dom";
+import NotFound from "./Pages/NotFound";
+import Checkout from "./Pages/Checkout";
+import Product from "./Pages/Product";
 function App() {
   return (
-    null
+    <Routes>
+      <Route path="*" element={<NotFound />} />
+      <Route path="/" element={<ShoppingList />} />
+      <Route path="/checkout" element={<Checkout />} />
+      <Route path="/product/:id" element={<Product />} />
+    </Routes>
   );
 }
 
